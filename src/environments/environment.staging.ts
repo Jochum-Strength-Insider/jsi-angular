14499f4379b341bac04c70d96683000fc70cbc44
seanneppl <sean.e.neppl@gmail.com>
2023-03-12 21:48:28 -0500

    add staging environment

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  firebase: {
+    projectId: process.env['PROJECT_ID_DEV'],
+    appId: process.env['APP_ID_DEV'],
+    databaseURL: process.env['DATABASE_URL_DEV'],
+    storageBucket: process.env['STORATE_BUCKET_DEV'],
+    locationId: process.env['LOCATION_ID_DEV'],
+    apiKey: process.env['API_KEY_DEV'],
+    authDomain: process.env['AUTH_DOMAIN_DEV'],
+    messagingSenderId: process.env['MESSAGING_SENDER_ID_DEV']
+  },
+  production: false,
+  hmr: false,
+  version: '1.0',
+  serverUrl: 'https://api.jochumstrengthinsider.com',
+  test: process.env['TEST_STAGING']
+};
