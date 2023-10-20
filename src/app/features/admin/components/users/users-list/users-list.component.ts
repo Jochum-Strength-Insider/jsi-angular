04682780f1a8ab5ecb4c081b4c82a680dff001b2
seanneppl <sean.e.neppl@gmail.com>
2023-10-20 00:25:07 -0500

    user sort and responsive list-items

--- src/app/features/admin/components/users/users-list/users-list.component.ts
+++ src/app/features/admin/components/users/users-list/users-list.component.ts
@@ -4,7 +4,6 @@ import { User, UserKeys, UserWithSelection } from '@app/@core/models/auth/user.m
 import { ifPropChanged } from '@app/@core/utilities/property-changed.utilities';
 import { Subscription } from 'rxjs';
 
-
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
@@ -42,7 +41,7 @@ export class UsersListComponent implements OnInit, OnChanges {
     this.filterUsers();
     this.searchSub = this.searchCtrl.valueChanges
       .subscribe((value) => {
-        this.search = value;
+        this.search = value.toLocaleLowerCase();
         this.filterUsers();
       })
   }
@@ -55,6 +54,25 @@ export class UsersListComponent implements OnInit, OnChanges {
 
   ngOnDestroy() {
     this.searchSub?.unsubscribe();
+  }
+
+  addUser(){
+    this.addUserClicked.emit();
+    this.show = false;
+  }
+
+  setCurrentUser(user: User){
+    this.userClicked.emit(user);
+  }
+
+  sendUserMessages() {
+    this.sendGroupMessagesClicked.emit(this.selectedUsers);
+    this.show = false;
+  }
+
+  setUserPrograms() {
+    this.setGroupProgramsClicked.emit(this.selectedUsers);
+    this.show = false;
   }
 
   showAllUsers() {
@@ -78,35 +96,6 @@ export class UsersListComponent implements OnInit, OnChanges {
     this.show = false;
   }
 
-  private filterUsers() {
-    let filteredUsers = this.users;
-    if(this.filter !== null) {
-      filteredUsers = this.getFilteredUsers(filteredUsers, this.filter);
-    }
-    filteredUsers = filteredUsers.filter(user => user.username.includes(this.search) || user.email.includes(this.search))
-    let sortedUsers = this.getSortUsers(filteredUsers, this.asc, this.currentSortProperty);
-    this.sortedUsers = sortedUsers;
-  }
-
-  private getFilteredUsers(array: UserWithSelection[], filter: boolean): UserWithSelection[] {
-    return array.filter((user: UserWithSelection) => user.ACTIVE === filter);
-  }
-
-  private getSortUsers(array: UserWithSelection[], asc: boolean, property: UserKeys): UserWithSelection[] {
-    const fx = (a: UserWithSelection, b: UserWithSelection) => asc ? (a[property] > b[property] ? 1 : -1) : (a[property] < b[property] ? 1 : -1);
-    return array.sort(fx);
-  }
-
-  sendUserMessages() {
-    this.sendGroupMessagesClicked.emit(this.selectedUsers);
-    this.show = false;
-  }
-
-  setUserPrograms() {
-    this.setGroupProgramsClicked.emit(this.selectedUsers);
-    this.show = false;
-  }
-
   selectionChanged(user: User) {
     const objWithIdIndex = this.selectedUsers.findIndex((u) => u.id === user.id);
     if (objWithIdIndex > -1) {
@@ -116,17 +105,56 @@ export class UsersListComponent implements OnInit, OnChanges {
     }
   }
 
-  addUser(){
-    this.addUserClicked.emit();
-    this.show = false;
-  }
-
-  setCurrentUser(user: User){
-    this.userClicked.emit(user);
-  }
-
   clearSelection(){
     this.sortedUsers.forEach(each => each.checked = false);
     this.show = false;
+  }
+
+  private filterUsers() {
+    let filteredUsers = this.users;
+
+    if(this.filter !== null) {
+      filteredUsers = this.getFilteredUsers(filteredUsers, this.filter);
+    }
+    filteredUsers = filteredUsers
+      .filter(
+        user => user.username.toLocaleLowerCase().includes(this.search) 
+        || user.email.toLocaleLowerCase().includes(this.search)
+      )
+
+    this.sortedUsers = this.getSortedUsers(filteredUsers, this.asc, this.currentSortProperty);
+  }
+
+  private getFilteredUsers(array: UserWithSelection[], filter: boolean): UserWithSelection[] {
+    return array.filter((user: UserWithSelection) => user.ACTIVE === filter);
+  }
+
+  private getSortedUsers(array: UserWithSelection[], asc: boolean, property: UserKeys) {
+    let sortFunction = (a: UserWithSelection, b: UserWithSelection) => 0
+    switch (property) {
+      case 'username':
+      case 'email':
+        sortFunction = (a: UserWithSelection, b: UserWithSelection) => {
+          const aValue = a[property].toLocaleLowerCase();
+          const bValue = b[property].toLocaleLowerCase();
+          return asc
+            ? (aValue < bValue) ? 1 : (aValue > bValue) ? -1 : 0
+            : (bValue < aValue) ? 1 : (bValue > aValue) ? -1 : 0
+        }
+        break;
+      case 'createdAt':
+      case 'createdAt':
+      case 'programDate':
+        sortFunction = (a: UserWithSelection, b: UserWithSelection) => {
+          const aValue = a[property] || 0;
+          const bValue = b[property] || 0;
+          return asc ? (aValue - bValue) : (bValue - aValue)
+        }       
+        break;    
+      default:
+        sortFunction = (a: UserWithSelection, b: UserWithSelection) => 0
+        break;
+    }
+    return array.sort(sortFunction);
   }
 }
