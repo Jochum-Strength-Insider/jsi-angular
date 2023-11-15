dcf66971b8ebf213e7de2e0aef82cd353c54b0e3
seanneppl <sean.e.neppl@gmail.com>
2023-11-14 22:12:30 -0600

    reword cancellation message

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,7 +12,7 @@ export const environment = {
     confirmationEmailRedirect: process.env['CONFIRMATION_EMAIL_REDIRECT_PROD']
   },
   production: true,
-  version: '1.0.3',
+  version: '1.0.4',
   serverUrl: 'https://api.jochumstrengthinsider.com',
   crytoKey: 'Ins1d3r!',
   login: '',
